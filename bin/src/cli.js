1f06300b1693667e5c56e3c6180b4b06b0e9e69c
vladimirskayatamara <vladimirskayatamara@gmail.com>
2023-03-01 15:09:44 +0400

    Linter setup

--- bin/src/cli.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-
-export default function sayHi() {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    const hiName = `Hello, ${name}!`;
-    console.log(hiName);
-    return name;
-}
